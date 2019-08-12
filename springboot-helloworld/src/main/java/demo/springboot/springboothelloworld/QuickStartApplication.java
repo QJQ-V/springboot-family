a07077c829720b25448ebd06088f1f0fba93dc55
QJQ <1062253554@qq.com>
2019-08-12 18:35:23 +0800

    1. 新增Helloworld Demo 2. 新增Restful Demo 3. 新增Mybatis Demo

---
+++ springboot-helloworld/src/main/java/demo/springboot/springboothelloworld/QuickStartApplication.java
@@ -0,0 +1,13 @@
+package demo.springboot.springboothelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickStartApplication.class, args);
+    }
+
+}
---
+++ springboot-restful/src/main/java/demo/springboot/springbootrestful/SpringbootRestfulApplication.java
@@ -0,0 +1,13 @@
+package demo.springboot.springbootrestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootRestfulApplication.class, args);
+    }
+
+}
