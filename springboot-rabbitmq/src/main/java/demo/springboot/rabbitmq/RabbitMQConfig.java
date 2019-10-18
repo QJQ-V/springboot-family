bfec87791ef7146815606269cbc67ca18e5dc828
QJQ <1062253554@qq.com>
2019-10-18 18:18:36 +0800

    更新MQ实现

--- springboot-rabbitmq/src/main/java/demo/springboot/rabbitmq/RabbitMQConfig.java
+++ springboot-rabbitmq/src/main/java/demo/springboot/rabbitmq/RabbitMQConfig.java
@@ -1,15 +1,71 @@
 package demo.springboot.rabbitmq;
 
-import org.springframework.amqp.core.Queue;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
-    public static final String QUEUE_NAME = "spring-boot-simple";
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    // fanout
+    public static final String FANOUT_QUEUE1 = "fanout.queue1";
+    public static final String FANOUT_QUEUE2 = "fanout.queue2";
+    public static final String FANOUT_EXCHANGE = "fanout.exchange";
+
+    // direct
+    public static final String DIRECT_QUEUE1 = "direct.queue1";
+    public static final String DIRECT_QUEUE2 = "direct.queue2";
+    public static final String DIRECT_EXCHANGE = "direct.exchange";
+
+    // topic
+    public static final String TOPIC_QUEUE1 = "topic.queue1";
+    public static final String TOPIC_QUEUE2 = "topic.queue2";
+    public static final String TOPIC_EXCHANGE = "topic.exchange";
+
+
+   // Topic模式
+    @Bean
+    public Queue topicQueue1() {
+        return new Queue(TOPIC_QUEUE1);
+    }
 
     @Bean
-    public Queue queue() {
-        return new Queue(QUEUE_NAME);
+    public TopicExchange topicExchange() {
+        return new TopicExchange(TOPIC_EXCHANGE);
+    }
+
+    @Bean
+    public Binding topicBinding1() {
+        return BindingBuilder.bind(topicQueue1()).to(topicExchange()).with("lzc.message");
+    }
+
+    // fanout模式
+    @Bean
+    public Queue fanoutQueue1() {
+        return new Queue(FANOUT_QUEUE1);
+    }
+
+    @Bean
+    public FanoutExchange fanoutExchange() {
+        return new FanoutExchange(TOPIC_EXCHANGE);
+    }
+
+    @Bean
+    public Binding fanoutBinding1() {
+        return BindingBuilder.bind(fanoutQueue1()).to(fanoutExchange());
+    }
+
+    // direct模式
+    @Bean
+    public Queue directQueue1() {
+        return new Queue(DIRECT_QUEUE1);
+    }
+
+    @Bean
+    public DirectExchange directExchange() {
+        return new DirectExchange(DIRECT_EXCHANGE);
     }
 }
